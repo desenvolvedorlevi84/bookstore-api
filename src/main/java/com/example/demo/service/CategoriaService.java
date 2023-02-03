ec3a063483bb34124ea49687f00ad506cc3bdab9
desenvolvedorlevi84 <isaquelevi@gmail.com>
2023-02-03 15:04:38 -0300

    UPDATE para categoria

--- src/main/java/com/example/demo/service/CategoriaService.java
+++ src/main/java/com/example/demo/service/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.domain.Categoria;
+import com.example.demo.dtos.CategoriaDTO;
 import com.example.demo.repositories.CategoriaRepository;
 import com.example.demo.service.exceptions.ObjectNotFoundException;
 
@@ -31,6 +32,14 @@ public class CategoriaService {
 	public Categoria create(Categoria obj) {
 		obj.setId(null);
 		return repository.save(obj);
+	}
+
+	public Categoria update(Integer id, CategoriaDTO objDto) {
+		Categoria obj = findById(id);
+		obj.setNome(objDto.getNome());
+		obj.setDescricao(objDto.getDescricao());
+		return repository.save(obj);
+		
 	}
 
 }
