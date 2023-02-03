d40d4b628b4e65717e0e08a24b509b4651b39fad
desenvolvedorlevi84 <isaquelevi@gmail.com>
2023-02-03 16:50:20 -0300

    GET para livro por ID

---
+++ src/main/java/com/example/demo/service/LivroService.java
@@ -0,0 +1,25 @@
+package com.example.demo.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.domain.Livro;
+import com.example.demo.repositories.LivroRepository;
+import com.example.demo.service.exceptions.ObjectNotFoundException;
+
+@Service
+public class LivroService {
+
+	@Autowired
+	private LivroRepository repository;
+
+	public Livro findById(Integer id) {
+		Optional<Livro> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Livro.class.getName()));
+
+	}
+
+}
