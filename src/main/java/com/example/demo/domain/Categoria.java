84f4f0f64e39888e4c9a7d5ce49b48055f3fa976
desenvolvedorlevi84 <isaquelevi@gmail.com>
2023-02-03 20:32:36 -0300

    ajustes finalizados no backend

--- src/main/java/com/example/demo/domain/Categoria.java
+++ src/main/java/com/example/demo/domain/Categoria.java
@@ -3,11 +3,15 @@ package com.example.demo.domain;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.hibernate.validator.constraints.Length;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.NotEmpty;
 
 @Entity
 public class Categoria implements Serializable {
@@ -17,7 +21,13 @@ public class Categoria implements Serializable {
 	@Id
 	@GeneratedValue (strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
+	@NotEmpty(message = "Campo NOME é requerido")
+	@Length(min = 3, max = 100, message = "O campo NOME deve ter entre 3 e 100 caracteres")
 	private String nome;
+	
+	@NotEmpty(message = "Campo DESCRIÇÃO é requerido")
+	@Length(min = 3, max = 200, message = "O campo DESCRIÇÃO deve ter entre 3 e 200 caracteres")
 	private String descricao;
 
 	@OneToMany (mappedBy = "categoria")
--- src/main/java/com/example/demo/domain/Livro.java
+++ src/main/java/com/example/demo/domain/Livro.java
@@ -2,6 +2,8 @@ package com.example.demo.domain;
 
 import java.io.Serializable;
 
+import org.hibernate.validator.constraints.Length;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
@@ -10,6 +12,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotEmpty;
 
 @Entity
 public class Livro implements Serializable {
@@ -19,8 +22,17 @@ public class Livro implements Serializable {
 	@Id
 	@GeneratedValue (strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
+	@NotEmpty(message = "Campo TITULO é requerido")
+	@Length(min = 3, max = 50, message = "O campo TITULO deve ter entre 3 e 50 caracteres")
 	private String titulo;
+	
+	@NotEmpty(message = "Campo NOME DO AUTOR é requerido")
+	@Length(min = 3, max = 50, message = "O campo NOME DO AUTOR deve ter entre 3 e 50 caracteres")
 	private String nome_autor;
+	
+	@NotEmpty(message = "Campo TEXTO é requerido")
+	@Length(min = 10, max = 2000000, message = "O campo TEXTO deve ter entre 10 e 2.000.000 caracteres")
 	private String texto;
 
 	@JsonIgnore
