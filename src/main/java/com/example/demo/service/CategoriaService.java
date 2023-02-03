dac54806a9d5e0e10eec024bf96e94f64c978a06
desenvolvedorlevi84 <isaquelevi@gmail.com>
2023-02-03 16:09:59 -0300

    Excecao personalizada para Delete de categoria

--- src/main/java/com/example/demo/service/CategoriaService.java
+++ src/main/java/com/example/demo/service/CategoriaService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 import com.example.demo.domain.Categoria;
 import com.example.demo.dtos.CategoriaDTO;
 import com.example.demo.repositories.CategoriaRepository;
+import com.example.demo.service.exceptions.DataIntegrityViolationException;
 import com.example.demo.service.exceptions.ObjectNotFoundException;
 
 @Service
@@ -23,12 +24,12 @@ public class CategoriaService {
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 
 	}
-	
-	public List<Categoria> findAll(){
+
+	public List<Categoria> findAll() {
 		return repository.findAll();
-		
+
 	}
-	
+
 	public Categoria create(Categoria obj) {
 		obj.setId(null);
 		return repository.save(obj);
@@ -39,12 +40,16 @@ public class CategoriaService {
 		obj.setNome(objDto.getNome());
 		obj.setDescricao(objDto.getDescricao());
 		return repository.save(obj);
-		
+
 	}
 
 	public void delete(Integer id) {
 		findById(id);
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new com.example.demo.service.exceptions.DataIntegrityViolationException("Categoria nao pode ser deletada! Possui livros associados");
+		}
 	}
 
 }
