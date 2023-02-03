abe2d5665cc2995ce8dc7d25beef941a7e56a5aa
desenvolvedorlevi84 <isaquelevi@gmail.com>
2023-02-02 22:10:22 -0300

    instancia de forma automatica da base de dados

--- src/main/java/com/example/demo/repositories/LivroRepository.java
+++ src/main/java/com/example/demo/repositories/LivroRepository.java
@@ -1,7 +1,6 @@
 package com.example.demo.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.example.demo.domain.Livro;
 
 public interface LivroRepository extends JpaRepository<Livro, Integer> {
